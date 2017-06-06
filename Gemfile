36694b68dc552b50d142815d1196615f68e27b81
Brandon <booth.brandon4@gmail.com>
2017-06-06 14:00:11 -0400

    can scrape names but only the same name many times

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'twitter-typeahead-rails'
 
 gem 'mechanize', '~> 2.7', '>= 2.7.5'
 
+gem 'rubysl-open-uri'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,4 +1,30 @@
 class ScraperController < ApplicationController
+
   def show
+    mechanize = Mechanize.new
+
+    meetup = mechanize.get('https://www.meetup.com/NYC-Social/members/')
+    $max = 10
+    $counter = 0
+
+    @names = []
+
+    while $counter <= $max do
+      @names.push(meetup.at('.memName').text.strip)
+      $counter += 1
+    end
+
+    # uri = File.read("https://www.meetup.com/NYC-Social/members/")
+ 
+    # # Use Nokogiri to get the document
+    # doc = Nokogiri::HTML(uri)
+    
+    # # Find the link of interest
+    # link = doc.search('.memName')
+    
+    # # Emit the content associated with that link
+    # puts link.content
+
   end
+
 end
