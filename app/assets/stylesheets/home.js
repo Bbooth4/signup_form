c6041b3077f69a9e98df789a86479bf70a0a8e10
Brandon <booth.brandon4@gmail.com>
2017-06-05 20:00:11 -0400

    super rough home page

--- app/assets/stylesheets/home.js
+++ app/assets/stylesheets/home.js
@@ -1,15 +1,30 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  'rownavbar': {
-    'width': [{ 'unit': '%H', 'value': 1 }]
-  },
   'row': {
     'marginRight': [{ 'unit': 'px', 'value': 0 }],
     'marginLeft': [{ 'unit': 'px', 'value': 0 }],
     'maxWidth': [{ 'unit': 'rem', 'value': 200 }]
   },
-  'group': {
-    'width': [{ 'unit': '%H', 'value': 1 }]
+  'logo': {
+    'height': [{ 'unit': 'em', 'value': 5.4 }],
+    'width': [{ 'unit': 'em', 'value': 16 }]
+  },
+  'icons': {
+    'margin': [{ 'unit': 'em', 'value': 0 }, { 'unit': 'em', 'value': 30 }, { 'unit': 'em', 'value': 0 }, { 'unit': 'em', 'value': 30 }]
+  },
+  'icon': {
+    'height': [{ 'unit': 'em', 'value': 10 }],
+    'width': [{ 'unit': 'em', 'value': 10 }]
+  },
+  'expand': {
+    'borderRadius': '20px'
+  },
+  'bottom-button': {
+    'color': '#fff'
+  },
+  'bottom': {
+    'marginTop': [{ 'unit': 'em', 'value': 10 }],
+    'textAlign': 'center'
   }
 });
