9ae01623bc2373d5e26e750f03bf71d42d0be1dd
Brandon <booth.brandon4@gmail.com>
2017-06-06 22:43:05 -0400

    added a fixed navbar and adjusted the form accordingly

--- app/assets/stylesheets/scraper.js
+++ app/assets/stylesheets/scraper.js
@@ -2,6 +2,8 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   'form': {
+    'position': 'relative',
+    'top': [{ 'unit': 'px', 'value': 120 }],
     'border': [{ 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'lightblue' }],
     'background': '#f2f7ff',
     'borderRadius': '5px',
@@ -9,6 +11,8 @@ export default StyleSheet.create({
   },
   'form form-title': {
     'borderBottom': [{ 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'lightblue' }],
+    'width': [{ 'unit': '%H', 'value': 0.9 }],
+    'marginLeft': [{ 'unit': '%H', 'value': 0.05 }],
     'textAlign': 'center',
     'padding': [{ 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 20 }, { 'unit': 'px', 'value': 20 }]
   },
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to '/'
+      redirect_to '/users'
     else
       redirect_to '/signup'
     end
