795f3dd39e968880f56572532d6ab015135f0967
Brandon <booth.brandon4@gmail.com>
2017-06-07 15:58:36 -0400

    fixed delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
 
   autocomplete :user, :email
 
+  # it used to auto complete, don't know what happened but I ran out of time to figure it out
+
   def load_suggestions
     @suggestions = MyModel.select(:email)
     render json: @suggestions
@@ -37,9 +39,31 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:id] = @user.id
-      redirect_to '/users/:id'
+      redirect_to :users
     else
       redirect_to '/signup'
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
